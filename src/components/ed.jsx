64d57459c151678c45bbca2dd3c161074a5449a8
Kyongaya <Kyongaya@yahoo.com>
2023-10-26 14:57:29 -0400

    10/26/2023

--- src/components/ed.jsx
+++ src/components/ed.jsx
@@ -9,10 +9,10 @@ const ed = () => {
      <div className="container vk-bg-container" >
      <div className="row p4-4 ">
                     <div className="col-md-12 py-4">
-                      <h1 className="text-primary fw-bold mb-4 text-center  vk-title1"> 에드 씬트로넬리 교회 </h1>
+                      <h1 className="text-primary fw-bold mb-4 text-center  vk-title1">  </h1>
                         <div className='text-white py-4 church-introduction'>
-<p>강력한 성령사역 예언, 치유, 축사 </p>
-<p>예수님 증거하는 교회</p> 
+<p>강력한 성령사역</p><p>예언, 치유, 축사 </p>
+<p>문제 해결과 놀라운 기적이 있는곳</p> 
 {/*   
  <p>  이 교회는 성령님의 인도아래, 씨니어 예언자이신 에드 씬트로넬리께서 지휘 하시고 쥬이어 예언자이신 조지 땡찬은 뉴욕에서 사역 하시는 분입니다. 
 
@@ -25,27 +25,30 @@ const ed = () => {
 </div></div>
 
      </section>
-        {/* <section className="bg-fire img-fluid"> 
+        <section className=""> 
      
      <div className="container vk-bg-container" >
      
                 <div className="row p4-4 ">
                     <div className="col-md-12 py-4">
-                        <h1 className="text-primary fw-bold mb-4 text-center vk-title1"> 에드 씬트로넬리 교회 </h1>
+                        <h1 className="text-primary fw-bold mb-4 text-center vk-title1"> 5중(사도, 선지자, 전도자, 목사,선생님) 사역 교회 </h1>
+                    <p>교회에 가서 지쳐진 영혼들이 왔는데도 주님의 답이 없어서 실망하지않어셨습니까?</p>
+                  <p>정말 하나님이 살아 계시는 걸까요? 왜 방언 기도를 해야 하는 거죠? 사단의 공격을 어떻게 방어하는 거죠? 그리고 죽고 나면 모든게 끝나는 것이 아닌가요? </p>
+                  <p>왜 예수님을 구원자로 맞아들여야 하는거죠? 왜 성령님이 필요한 거죠?</p>
+                  <p>왜 자유스럽게 인생을 마음대로 살수 없는 거죠?</p>
+                  <p>이러한 궁금증을 성령님의 나타나심으로 해결해 드립니다. 살아서 역사하는 능력있는 교회에 오셔요.</p>
 <p> 저의 휍사이트 방문을 감사드립니다. 이 교회는 사도 바울이 사역하신 치유, 축사, 예언 이 있는 살아서 역사하는 성령 사역이 있는 교회로써
 하나님의 말씀이 살아서 움직이는 교회입니다.</p>
   
-<p>  이 교회는 성령님의 인도아래, 씨니어 예언자이신 에드 씬트로넬리께서 지휘 하시고 쥬이어 예언자이신 조지 땡찬은 뉴욕에서 사역 하시는 분입니다. 
-
-예언자 에드님은   성령님의 인도 하심에 따라 미국 여러곳을 집회 
-뉴욕, 펜셀베니아, 시카고, 캘리포니아, 플로리다, 미시건, 여러 곳을 순회하시고, 세계여러나라들 순회 하십니다. </p>
+<p>이 교회는 성령님의 인도아래, 씨니어 예언자이신 에드 씬트로넬리께서 지휘 하시고 지금 텍사스에 사역하시고, 그의 영적 아들, 쥬이어 예언자이신 조지 땡찬은 뉴욕에서 사역 하시는 분입니다. 선지자 에드님은   성령님의 인도 하심에 따라 미국 여러곳을 집회 
+뉴욕, 펜셀베니아, 시카고, 캘리포니아, 플로리다, 미시건,캔터키 여러 곳을 순회하시고, 세계여러나라들을 순회 하십니다. </p>
  
 <p>제가 당신을 에드 씬트로넬리 사역장에 초대하고 싶습니다. 만일 여러분이 에드 씬트로넬리 사역에 관심이 있어시면, 연락하시길 바랍니다.</p>
-
+<h2>718-708-1363</h2>
 </div>
 ​</div></div>    
         
-        </section> */}
+        </section>
        <div className="container py-5">
         <div class="row py-5">
     <div class="col-xs-12 col-md-6">
