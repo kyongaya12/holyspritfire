df70c8df8d9c65e49139463d358e9755d1f4485b
kyongaya12 <106499025+kyongaya12@users.noreply.github.com>
2024-01-15 15:13:22 -0500

    jan 15 3:13

--- src/components/BgVideo.jsx
+++ src/components/BgVideo.jsx
@@ -7,13 +7,16 @@ const BgVideo = () => {
     <div className="bgContainer">
            
         <div className="overlay">
-        <video src={videoSrc} type="video/mp4" autoPlay loop muted />   
-        {/* <video controls autoPlay loop muted >       
-        <source src={videoSrc} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video> */}
+        {/* <video src={videoSrc} type="video/mp4" autoPlay loop muted />    */}
+        {/* <video className='video-bg' autoPlay loop muted>
+        <source src="./images/video1.mp4" type="video/mp4"/>
+        </video>  */}
+
+
+       
+
     
-      {/* <iframe width="100%" height="100%" src="https://youtube.com/embed/dvRdMU-NyJg?autoplay=1&loop=1" title="YouTube video player" frameborder="0" allowfullscreen></iframe> */}
+      <iframe width="100%" height="100%" src="https://youtube.com/embed/dvRdMU-NyJg?autoplay=1&loop=1&mute=1" title="YouTube video player" frameborder="0" allowfullscreen></iframe>
             <div className="container-vk">
                 <h1 className="title">에드 시츠로넬리</h1>
                 <h2 className="wlc">강력한 성령사역</h2>
